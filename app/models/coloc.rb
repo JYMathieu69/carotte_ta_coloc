e2b3e62d885c9d13326114a7b9556d56981a9fcb
Mathieu Jolly <mathieujolly69@gmail.com>
2021-07-06 16:46:11 +0200

    Established all associations between Models

--- app/models/coloc.rb
+++ app/models/coloc.rb
@@ -1,3 +1,7 @@
 class Coloc < ApplicationRecord
-  belongs_to :user
+  belongs_to :leader, class_name: "User"
+  
+  has_many :users
+  has_many :coloc_tasks
+  has_many :ongoing_tasks, through: :coloc_tasks
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Vote.delete_all
+CarottedTask.delete_all
+OngoingTask.delete_all
+ColocTask.delete_all
+Coloc.delete_all
+Task.delete_all
+User.delete_all
+
+user1 = User.create!(name: "user1", email: "user1@toto.com", password: "azerty")
+coloc1 = Coloc.create!(name: "coloc1", leader_id: user1.id)
+task1 = Task.create!(name: "task1")
+coloc_task1 = ColocTask.create!(coloc_id: coloc1.id, task_id: task1.id)
+ongoing_task1 = OngoingTask.create!(coloc_task_id: coloc_task1.id, user_id: user1.id)
+carotted_task1 = CarottedTask.create!(ongoing_task_id: ongoing_task1.id, user_id: user1.id, carotted_user_id: user1.id)
+vote1 = Vote.create!(validated: true, ongoing_task_id: ongoing_task1.id, user_id: user1.id)
+vote2 = Vote.create!(validated: false, ongoing_task_id: ongoing_task1.id, user_id: user1.id)
