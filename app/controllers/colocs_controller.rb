449a4d6c5039e2c684a7d923bd6eca7c245f0287
Mickael FOSSO <fosso.mickael@gmail.com>
2021-07-08 16:26:34 +0200

    Colocs Controller

---
+++ app/controllers/colocs_controller.rb
@@ -0,0 +1,27 @@
+class ColocsController < ApplicationController
+
+    def new
+      @coloc = Coloc.new
+    end 
+
+    def create
+      @coloc = Coloc.new(coloc_params)
+      @coloc.leader = current_user
+      if @coloc.save
+        redirect_to coloc_path(@coloc)
+      else
+        render :new
+      end
+    end
+
+    def show
+        @coloc = Coloc.includes(:ongoing_tasks).find(params[:id])
+    end
+
+    private
+
+    def post_params
+      params.require(:coloc).permit(:name)
+    end
+  
+end
