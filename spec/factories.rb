2b149ce5181e2e00e30ef5c33a7c85468567dfdd
Mathieu Jolly <mathieujolly69@gmail.com>
2021-07-12 17:26:25 +0200

    First step RSpec

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,10 +1,7 @@
 FactoryBot.define do
     factory :user do
-        first_name   { 'Jean' }
-        last_name    { 'Toto' }
+        name   { 'Jean' }
         email        { 'toto@gmail.com' }
-        phone_number { '0695500784' }
-        is_entrepreneur { true }
         password     { 'azerty' }
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,22 +1,30 @@
 require 'rails_helper'
 describe User, type: :model do
-    context 'Associations' do
-        it { should have_many(:projects) } 
-        it { should have_many(:workshop_bookings) } 
-    end
+    # context 'Associations' do
+    #     it { should have_many(:projects) } 
+    #     it { should have_many(:workshop_bookings) } 
+    # end
     context 'Validations' do
-        it "is valid with a first name, last name, email, and a balance password" do
-            user = User.create(first_name: "Jean",
-                                last_name: "Krupa",
-                                email: "jkrupa@hotmail.fr",
-                                phone_number: "0695500784",
-                                password:"azerty")
+        it "is valid with a name, email, and a  password" do
+            user = User.create(name: "Jean",
+                                email: "jkrupa@hot.fr",
+                                password: "azerty")
+            p user
             expect(user).to be_valid
         end
-        it "is invalid without a first name" do
-            user = User.new(first_name: nil)
+        it "is invalid without a name" do
+            user = User.new(name: nil)
             user.valid?
-            expect(user.errors[:first_name]).to include('can\'t be blank')
+            p user.errors[:name]
+            expect(user.errors[:name]).to include('can\'t be blank')
         end
+    end
+
+    context 'methods' do 
+        it "is suppose to respond toto" do 
+            user = create(:user)
+            expect(user.full_name).to eq("toto")
+        end
+        
     end
 end
