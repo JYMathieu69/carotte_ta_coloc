af8698b4ac7c00b5466e299b081ebd239dce4cd2
Fanny Mugnier <fanny.mugnier@outlook.com>
2021-09-07 14:56:15 +0200

    create choose_tasks action instead of edit

--- app/controllers/colocs_controller.rb
+++ app/controllers/colocs_controller.rb
@@ -1,56 +1,58 @@
 class ColocsController < ApplicationController
+  before_action :set_coloc, only: [:edit, :update, :recap, :invitation, :choose_tasks]
 
-   before_action :set_coloc, only: [:edit, :update, :recap, :invitation]
+  def new
+    @coloc = Coloc.new
+  end
 
-   def new
-      @coloc = Coloc.new
-   end
+  def create
+    @coloc = Coloc.new(coloc_params)
+    @coloc.leader = current_user
+    current_user.coloc = @coloc
 
-   def create
-      
-      @coloc = Coloc.new(coloc_params)
-      @coloc.leader = current_user
-      current_user.coloc = @coloc
+    if @coloc.save && current_user.save
+      redirect_to choose_tasks_path(@coloc)
+    else
+      render :new
+    end
+  end
 
-      if @coloc.save && current_user.save
-         redirect_to edit_coloc_path(@coloc)
-      else
-         render :new
-      end
-   end
+  def choose_tasks
+    @tasks = Task.all
+    @tasks.each do |task|
+      @coloc.coloc_tasks.build(task: task, difficulty: task.default_difficulty)
+    end
+  end
 
-   def edit
-      @tasks = Task.all
-      @tasks.each do |task|
-         @coloc.coloc_tasks.build(task: task, difficulty: task.default_difficulty)
-      end
-   end
+  def edit; end
 
-   def recap
-   end
+  def recap; end
 
-   def join
-      @user = current_user
-   end
+  def join
+    @user = current_user
+  end
 
-   def invitation
-   end
+  def invitation; end
 
-   def update
-      if @coloc.update(coloc_params)
-         redirect_to "/colocs/#{@coloc.id}/recap"
-      else
-         render :edit
-      end
-   end
+  def update
+    if @coloc.update(coloc_params)
+      redirect_to is_completed? ? root_path : recap_path(@coloc)
+    else
+      render is_completed? ? :edit : :choose_tasks
+    end
+  end
 
-   private 
+  private
 
-   def coloc_params
-      params.require(:coloc).permit(:name, coloc_tasks_attributes: [:task_id, :difficulty, :coloc_task_ids])
-   end
+  def coloc_params
+    params.require(:coloc).permit(:name, coloc_tasks_attributes: [:task_id, :difficulty, :coloc_task_ids])
+  end
 
-   def set_coloc
-      @coloc = Coloc.find(params[:id])
-   end
+  def set_coloc
+    @coloc = Coloc.find(params[:id])
+  end
+
+  def is_completed?
+    @coloc.assignment_day
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,18 +9,17 @@ Rails.application.routes.draw do
   root to: 'ongoing_tasks#index'
 
   resources :users, only: [:update]
-  resources :colocs, only: [:new, :create, :edit, :update] do
-    resources :coloc_tasks, only: [:new, :create], as: 'tasks'
-  end
+  resources :colocs, only: [:new, :create, :edit, :update]
 
-  get '/colocs/:id/recap', to: 'colocs#recap'
+  get '/colocs/:id/choose_tasks', to: 'colocs#choose_tasks', as: 'choose_tasks'
+  get '/colocs/:id/recap', to: 'colocs#recap', as: 'recap'
   get '/colocs/:id/invitation', to: 'colocs#invitation'
   get '/home', to: 'pages#home'
   get '/join', to: 'colocs#join', as: 'join_coloc'
   post "start_ongoing_tasks", to: "ongoing_tasks#start_ongoing_tasks"
   get '/dashboard/:id/validate_task', to: 'ongoing_tasks#validate_task', as: 'validate_task'
   patch '/dashboard/:id/validation_update', to: 'ongoing_tasks#validation_update', as: 'validation_update'
-  
+
   get '/dashboard/:id/assign_task', to: 'ongoing_tasks#assign_task', as: 'assign_task'
   patch '/dashboard/:id/assign_task_update', to: 'ongoing_tasks#assign_task_update', as: 'assign_task_update'
   scope do
