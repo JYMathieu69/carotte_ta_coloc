2dedbbb77397731ac83110ccb5db1c9bea3b3e6d
Mickael FOSSO <fosso.mickael@gmail.com>
2021-09-09 15:35:52 +0200

    Show carroted view with recap: carroted user and congratulations

--- app/controllers/carotted_tasks_controller.rb
+++ app/controllers/carotted_tasks_controller.rb
@@ -6,11 +6,15 @@ class CarottedTasksController < ApplicationController
     return unless user_own_ongoing_task
     
     carotted_user = pick_carroted_user
-    CarottedTask.create(ongoing_task:  @ongoing_task , carotted_user: carotted_user, user: current_user)
+    carroted_task = CarottedTask.create(ongoing_task:  @ongoing_task , carotted_user: carotted_user, user: current_user)
     @ongoing_task.user = carotted_user
     @ongoing_task.save
     substract_carrots
-    redirect_to root_path
+    redirect_to carotted_task_path(carroted_task)
+  end
+
+  def show
+    @carroted_task = CarottedTask.find(params[:id])
   end
 
   private
