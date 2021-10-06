921412c5443d4de66d7fa412a3e89fc4c2735bcc
Mickael FOSSO <fosso.mickael@gmail.com>
2021-10-06 16:05:04 +0200

    file extension must be an image for photo before and photo after

--- app/services/daily_tasks_service.rb
+++ app/services/daily_tasks_service.rb
@@ -7,7 +7,7 @@ class DailyTasksService
   def call
     @daily_not_done_ongoing_tasks.each do |unassigned_ongoing_task|
       if unassigned_ongoing_task.finished?
-        OngoingTask.create(coloc_task_id: unassigned_ongoing_task.coloc_task.id)
+        OngoingTask.create!(coloc_task_id: unassigned_ongoing_task.coloc_task.id)
         unassigned_ongoing_task.done = true
       else
         UnfinishedTask.create(ongoing_task: unassigned_ongoing_task, user: unassigned_ongoing_task.user) if unassigned_ongoing_task.user
