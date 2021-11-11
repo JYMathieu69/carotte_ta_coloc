932980a2fc954919d23f50d1d0deb44e39d68d20
lboitier1 <lboitier1@gmail.com>
2021-11-11 17:08:45 +0100

    change name tachboard and add info bubble for each

---
+++ app/javascript/plugins/hovered_icon_text.js
@@ -0,0 +1,18 @@
+// const displayTextIconHovered = () => {
+//     const infoIcon = document.querySelector(".info-icon-home");
+//     const infoText = document.querySelector(".hovered_icon_text");
+
+//     infoIcon.addEventListener("mouseenter", function (event) {
+//         infoText.classList.remove("hide");
+//     });
+
+//     infoIcon.addEventListener("mouseleave", function (event) {
+//         infoText.classList.add("hide");
+//     });
+
+//     infoIcon.addEventListener("click", function (event) {
+//         infoText.classList.toggle("hide");
+//     });
+// }
+
+// export { displayTextIconHovered }
