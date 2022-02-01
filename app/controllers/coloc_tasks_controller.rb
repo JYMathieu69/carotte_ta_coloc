369fa4cf1cffb7c870440b5912fa02b47c17bc8a
Fanny Mugnier <fanny.mugnier@outlook.com>
2022-02-01 13:04:02 +0100

    add new screen to add coloc_tasks to coloc

--- app/controllers/coloc_tasks_controller.rb
+++ app/controllers/coloc_tasks_controller.rb
@@ -1,31 +1,41 @@
 class ColocTasksController < ApplicationController
   before_action :set_coloc_task, only: [:destroy]
+  before_action :set_coloc, only: [:new, :create, :destroy]
 
+  def new
+    existing_tasks = @coloc.coloc_tasks.map { |coloc_task| coloc_task.task_id }
+    @addable_tasks = Task.where.not(id: existing_tasks)
+    @new_coloc_task = ColocTask.new
+  end
+  
   def create
     items = params[:items]
 
     items.values.each do |item|
       if item["task"] != "0"
         new_coloc_task = ColocTask.new()
-        new_coloc_task.coloc = current_user.coloc
+        new_coloc_task.coloc = @coloc
         new_coloc_task.task = Task.find(item["task"])
-        render 'colocs/edit' if !new_coloc_task.save
+        render :new if !new_coloc_task.save
       end
     end
     
-    redirect_to edit_coloc_path(current_user.coloc)
+    redirect_to edit_coloc_path(@coloc)
   end
   
   def destroy
-    coloc = @coloc_task.coloc
     @coloc_task.destroy
 
-    redirect_to edit_coloc_path(coloc)
+    redirect_to edit_coloc_path(@coloc)
   end
 
   private
 
   def set_coloc_task
     @coloc_task = ColocTask.find(params[:id])
+  end
+
+  def set_coloc
+    @coloc = current_user.coloc
   end
 end
