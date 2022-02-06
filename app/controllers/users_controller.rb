18281842a00d0dfc0916ed7875ffaf41204c2e5a
Fanny Mugnier <fanny.mugnier@outlook.com>
2022-02-06 15:01:49 +0100

    refacto: move join_coloc in its own controller action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,30 @@
 class UsersController < ApplicationController
     def update
-        @user = current_user
-        token = params[:invite_token]
-        @user.coloc = Coloc.find_by(invite_token: token)
-
-        if @user.save && @user.coloc
-          if !@user.coloc.assignment_day
-            OnboardingChannel.broadcast_to(
-              @user.coloc,
-              {
-                user: current_user,
-                avatar_key: current_user.avatar.key
-              }
-            )
-          end
-          
-          redirect_to ongoing_tasks_path
-        else
-          render "colocs/join"
-        end
     end
-
+    
     def edit
       @user = current_user
+    end
+    
+    def join_coloc
+      @user = current_user
+      token = params[:invite_token]
+      @user.coloc = Coloc.find_by(invite_token: token)
+  
+      if @user.save && @user.coloc
+        if !@user.coloc.assignment_day
+          OnboardingChannel.broadcast_to(
+            @user.coloc,
+            {
+              user: current_user,
+              avatar_key: current_user.avatar.key
+            }
+          )
+        end
+        
+        redirect_to ongoing_tasks_path
+      else
+        render "colocs/join"
+      end
     end
 end
