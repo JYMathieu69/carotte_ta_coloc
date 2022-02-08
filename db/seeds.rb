4cb499d025399347301bab555a3b6ca125c13dc2
Fanny Mugnier <fanny.mugnier@outlook.com>
2022-02-08 16:29:13 +0100

    style: task creation more readable in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -75,198 +75,331 @@ User.delete_all
 
 puts "Creating Tasks..."
 
-vaisselle       = Task.create!(name: "Faire la vaisselle",         description: "💦 Fais-la tremper à l'avance pour gagner du temps",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 2)
+vaisselle = Task.create!(
+  name: "Faire la vaisselle",
+  description: "💦 Fais-la tremper à l'avance pour gagner du temps",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 2)
 vaisselle_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383954/FAIRE_LA_VAISSELLE_chxam7.png')
 vaisselle.image.attach(io: vaisselle_image, filename: 'vaisselle.png', content_type: 'image/png')
 
-aspirateur      = Task.create!(name: "Passer l'aspirateur",        description: "🔌 Un peu de cannelle sur le sol pour parfumer ton appart'",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 3)
+
+aspirateur = Task.create!(
+  name: "Passer l'aspirateur",
+  description: "🔌 Un peu de cannelle sur le sol pour parfumer ton appart'",
+  recurrence: "weekly",
+  auto_assigned: true, 
+  default_difficulty: 3)
 aspirateur_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383956/PASSER_LASPI_scfezm.png')
 aspirateur.image.attach(io: aspirateur_image, filename: 'aspirateur.png', content_type: 'image/png')
 
 
-poussiere       = Task.create!(name: "Enlever les poussières",         description: "💧 Avec un chiffon humide, c'est le top !",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 4)
+poussiere = Task.create!(
+  name: "Enlever les poussières",
+  description: "💧 Avec un chiffon humide, c'est le top !",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 4)
 poussiere_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383957/POUSSIERE_f4fvvq.png')
 poussiere.image.attach(io: poussiere_image, filename: 'poussiere.png', content_type: 'image/png')
 
 
-vitre           = Task.create!(name: "Laver les vitres",            description: "🪟 Passe toujours la raclette dans le même sens",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 4)
+vitre = Task.create!(
+  name: "Laver les vitres",
+  description: "🪟 Passe toujours la raclette dans le même sens",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 4)
 vitre_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383955/LAVER_LES_VITRES_ar7jjz.png')
 vitre.image.attach(io: vitre_image, filename: 'vitre.png', content_type: 'image/png')
 
 
-serpillere      = Task.create!(name: "Passer la serpillère",         description: "🪑 Enlève tous les meubles avant",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 3)
+serpillere = Task.create!(
+  name: "Passer la serpillère",
+  description: "🪑 Enlève tous les meubles avant",
+  recurrence: "weekly",
+  auto_assigned: true, 
+  default_difficulty: 3)
 serpillere_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383958/PASSER_LA_SERPILLERE_paolmv.png')
 serpillere.image.attach(io: serpillere_image, filename: 'serpillere.png', content_type: 'image/png')
 
 
-sortir_poubelle = Task.create!(name: "Sortir la poubelle", description: "♻️ Pense au tri",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 1)
+sortir_poubelle = Task.create!(
+  name: "Sortir la poubelle",
+  description: "♻️ Pense au tri",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 1)
 sortir_poubelle_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383957/SORTIR_LES_POUBELLES_ooglyo.png')
 sortir_poubelle.image.attach(io: sortir_poubelle_image, filename: 'sortir_poubelle.png', content_type: 'image/png')
 
 
-lessive         = Task.create!(name: "Faire une lessive",            description: "🍋 Utilise du citron pour garder le blanc",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 1)
+lessive = Task.create!(
+  name: "Faire une lessive",
+  description: "🍋 Utilise du citron pour garder le blanc",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 1)
 lessive_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383955/MACHINE_A_LAVER_uvmxok.png')
 lessive.image.attach(io: lessive_image, filename: 'lessive.png', content_type: 'image/png')
 
 
-toilettes       = Task.create!(name: "Laver les toilettes",          description: "🧪 Bicarbonate + Vinaigre Blanc",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 3)
+toilettes = Task.create!(
+  name: "Laver les toilettes",
+  description: "🧪 Bicarbonate + Vinaigre Blanc",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 3)
 toilettes_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383956/LAVER_LES_WC_m2ktuu.png')
 toilettes.image.attach(io: toilettes_image, filename: 'toilettes.png', content_type: 'image/png')
 
 
-salle_de_bain   = Task.create!(name: "Laver la salle de bain",      description: "🍋 Du citron sur la robinetterie",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 4)
+salle_de_bain = Task.create!(
+  name: "Laver la salle de bain",
+  description: "🍋 Du citron sur la robinetterie",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 4)
 salle_de_bain_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383955/LAVER_LA_SDB_wcjmco.png')
 salle_de_bain.image.attach(io: salle_de_bain_image, filename: 'salle_de_bain.png', content_type: 'image/png')
 
 
-linge           = Task.create!(name: "Étendre le linge",              description: "👕 On veut un maximum de cintres !",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 2)
+linge = Task.create!(
+  name: "Étendre le linge",
+  description: "👕 On veut un maximum de cintres !",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 2)
 linge_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383954/ETENDRE_LE_LINGE_hywywa.png')
 linge.image.attach(io: linge_image, filename: 'linge.png', content_type: 'image/png')
 
 
-courses         = Task.create!(name: "Faire les courses",            description: "🛵 Interdit de se les faire livrer",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 3)
+courses = Task.create!(
+  name: "Faire les courses",
+  description: "🛵 Interdit de se les faire livrer",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 3)
 courses_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383955/FAIRE_LES_COURSES_r6bsr7.png')
 courses.image.attach(io: courses_image, filename: 'courses.png', content_type: 'image/png')
 
 
-plantes         = Task.create!(name: "Arroser les plantes",            description: "🌱 Un petit sytème de goutte à goutte ?",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 1)
+plantes = Task.create!(
+  name: "Arroser les plantes",
+  description: "🌱 Un petit sytème de goutte à goutte ?",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 1)
 plantes_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383954/ARROSER_LS_PLANTES_jarxpo.png')
 plantes.image.attach(io: plantes_image, filename: 'plantes.png', content_type: 'image/png')
 
 
-verre           = Task.create!(name: "Vider le verre",              description: "👩‍❤️‍👨 Avec un coloc', c'est plus simple",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 3)
+verre = Task.create!(
+  name: "Vider le verre",
+  description: "👩‍❤️‍👨 Avec un coloc', c'est plus simple",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 3)
 verre_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383957/VIDER_LE_VERRE_ezdy84.png')
 verre.image.attach(io: verre_image, filename: 'verre.png', content_type: 'image/png')
 
 
-cuisiner        = Task.create!(name: "Cuisiner",           description: "🍳 Fais-en un peu plus pour le lendemain midi",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 2)
+cuisiner = Task.create!(
+  name: "Cuisiner",
+  description: "🍳 Fais-en un peu plus pour le lendemain midi",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 2)
 cuisiner_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383954/CUISINIER_o5ju6m.png')
 cuisiner.image.attach(io: cuisiner_image, filename: 'cuisiner.png', content_type: 'image/png')
 
 
-lave_vaisselle  = Task.create!(name: "Vider le lave-vaisselle",     description: "⚠️ Évite le caoutchouc et le silicone",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 2)
+lave_vaisselle = Task.create!(
+  name: "Vider le lave-vaisselle",
+  description: "⚠️ Évite le caoutchouc et le silicone",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 2)
 lave_vaisselle_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383957/VIDER_LAVE_VAISSELLE_je6vkk.png')
 lave_vaisselle.image.attach(io: lave_vaisselle_image, filename: 'lave_vaisselle.png', content_type: 'image/png')
 
 
-frigo           = Task.create!(name: "Nettoyer le frigo",              description: "🧴 Combo : Vinaigre blanc avec du citron",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 4)
+frigo = Task.create!(
+  name: "Nettoyer le frigo",
+  description: "🧴 Combo : Vinaigre blanc avec du citron",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 4)
 frigo_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383955/LAVER_FRIGO_bydty1.png')
 frigo.image.attach(io: frigo_image, filename: 'frigo.png', content_type: 'image/png')
 
 
-balai           = Task.create!(name: "Passer le balai",              description: "🧹 Accorde-toi avec celui/celle qui passe la serpillère",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 2)
+balai = Task.create!(
+  name: "Passer le balai",
+  description: "🧹 Accorde-toi avec celui/celle qui passe la serpillère",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 2)
 balai_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383956/PASSER_BALAI_ytlxqd.png')
 balai.image.attach(io: balai_image, filename: 'balai.png', content_type: 'image/png')
 
-animal          = Task.create!(name: "Sortir l'animal",  description: "🦴 N'oublie pas les friandises",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 2)
+animal = Task.create!(
+  name: "Sortir l'animal",
+  description: "🦴 N'oublie pas les friandises",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 2)
 animal_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383957/SORTRIR_LANIMAL_x58vmn.png')
 animal.image.attach(io: animal_image, filename: 'animal.png', content_type: 'image/png')
 
 
-four            = Task.create!(name: "Nettoyer le four",               description: "🥘 Les plaques au lave-vaisselle",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 4)
+four = Task.create!(
+  name: "Nettoyer le four",
+  description: "🥘 Les plaques au lave-vaisselle",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 4)
 four_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383956/NETTOYERFOUR_ijivuf.png')
 four.image.attach(io: four_image, filename: 'four.png', content_type: 'image/png')
 
 
-placard         = Task.create!(name: "Nettoyer les placards",            description: "💧 Combo : eau chaude + liquide vaisselle",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 3)
+placard = Task.create!(
+  name: "Nettoyer les placards",
+  description: "💧 Combo : eau chaude + liquide vaisselle",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 3)
 placard_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383956/PLACARD_wfjqyn.png')
 placard.image.attach(io: placard_image, filename: 'placard.png', content_type: 'image/png')
 
 
-micro_onde      = Task.create!(name: "Nettoyer le micro-onde",         description: "🍋 Fais chauffer un bol d'eau citronnée pendant 2mn avant",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 2)
+micro_onde = Task.create!(
+  name: "Nettoyer le micro-ondes",
+  description: "🍋 Fais chauffer un bol d'eau citronnée pendant 2mn avant",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 2)
 micro_onde_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383956/NETTOYER_MICRONDE_fsrqsy.png')
 micro_onde.image.attach(io: micro_onde_image, filename: 'micro_onde.png', content_type: 'image/png')
 
 
-nettoyer_table  = Task.create!(name: "Nettoyer la table",  description: "🧽 Un petit chiffon humidifié",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 1)
+nettoyer_table = Task.create!(
+  name: "Nettoyer la table",
+  description: "🧽 Un petit chiffon humidifié",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 1)
 nettoyer_table_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383956/NETTOYER_TABLE_kouykm.png')
 nettoyer_table.image.attach(io: nettoyer_table_image, filename: 'nettoyer_table.png', content_type: 'image/png')
 
 
-pelouse         = Task.create!(name: "Tondre la pelouse",            description: "✋🏻 Évite le dimanche",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 4)
+pelouse = Task.create!(
+  name: "Tondre la pelouse",
+  description: "✋🏻 Évite le dimanche",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 4)
 pelouse_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383957/TONDRE_jthxbl.png')
 pelouse.image.attach(io: pelouse_image, filename: 'pelouse.png', content_type: 'image/png')
 
 
-papier          = Task.create!(name: "S'occuper des papiers",            description: "📝 Ah...l'administratif !",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 2)
+papier = Task.create!(
+  name: "S'occuper des papiers",
+  description: "📝 Ah...l'administratif !",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 2)
 papier_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636356160/VIDER_LE_VERRE_etdevf.png')
 papier.image.attach(io: papier_image, filename: 'papier.png', content_type: 'image/png')
 
 
-piece_commune   = Task.create!(name: "Ranger la pièce commune",      description: "🛋 Histoire de circuler détente",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 2)
+piece_commune = Task.create!(
+  name: "Ranger la pièce commune",
+  description: "🛋 Histoire de circuler détente",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 2)
 piece_commune_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383957/RANGER_PIECES_COMMUNES_ocplbh.png')
 piece_commune.image.attach(io: piece_commune_image, filename: 'piece_commune.png', content_type: 'image/png')
 
 
-cendrier        = Task.create!(name: "Vider le cendrier",           description: "🧯 Un peu d'eau au cas où",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 1)
+cendrier = Task.create!(
+  name: "Vider le cendrier",
+  description: "🧯 Un peu d'eau au cas où",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 1)
 cendrier_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383957/VIDER_CENDRIER_pzkh7f.png')
 cendrier.image.attach(io: cendrier_image, filename: 'cendrier.png', content_type: 'image/png')
 
 
-bouilloire      = Task.create!(name: "Détartrer la bouilloire",         description: "💦 Combo : eau + acide citrique",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 1)
+bouilloire = Task.create!(
+  name: "Détartrer la bouilloire",
+  description: "💦 Combo : eau + acide citrique",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 1)
 bouilloire_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383954/DETARTRER_BOUILLOIRE_jrlnm6.png')
 bouilloire.image.attach(io: bouilloire_image, filename: 'bouilloire.png', content_type: 'image/png')
 
 
-café            = Task.create!(name: "Laver la machine à café",               description: "☕️ Vinaigre à balle !",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 1)
+café = Task.create!(
+  name: "Laver la machine à café",
+  description: "☕️ Vinaigre à balle !",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 1)
 café_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383954/DETARTRER_CAFE_ghtzco.png')
 café.image.attach(io: café_image, filename: 'café.png', content_type: 'image/png')
 
 
-evier           = Task.create!(name: "Laver l'évier",              description: "💪🏻 Toujours et encore le bicarbonate de soude",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 1)
+evier = Task.create!(
+  name: "Laver l'évier",
+  description: "💪🏻 Toujours et encore le bicarbonate de soude",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 1)
 evier_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383955/EVIER_yizwcc.png')
 evier.image.attach(io: evier_image, filename: 'evier.png', content_type: 'image/png')
 
 
-mettre_table    = Task.create!(name: "Mettre la table",    description: "🍴 Couteau à droite, fourchette à gauche",
-recurrence: "daily",  auto_assigned: false, default_difficulty: 1)
+mettre_table = Task.create!(
+  name: "Mettre la table",
+  description: "🍴 Couteau à droite, fourchette à gauche",
+  recurrence: "daily",
+  auto_assigned: false,
+  default_difficulty: 1)
 mettre_table_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1642675677/METTRE_LA_TABLE_qcwwqe.png')
 mettre_table.image.attach(io: mettre_table_image, filename: 'mettre_table.png', content_type: 'image/png')
 
 
-laver_poubelle  = Task.create!(name: "Laver la poubelle",  description: "🧂 Mets un peu de sel au fond",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 2)
+laver_poubelle = Task.create!(
+  name: "Laver la poubelle",
+  description: "🧂 Mets un peu de sel au fond",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 2)
 laver_poubelle_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383955/LAVER_POUBELLES_pz5kq8.png')
 laver_poubelle.image.attach(io: laver_poubelle_image, filename: 'laver_poubelle.png', content_type: 'image/png')
 
 
-chat            = Task.create!(name: "Changer la litière du chat",    description: "😳 Bicarbonate de soude pour éviter les odeurs",
-recurrence: "weekly", auto_assigned: true,  default_difficulty: 2)
+chat = Task.create!(
+  name: "Changer la litière du chat",
+  description: "😳 Bicarbonate de soude pour éviter les odeurs",
+  recurrence: "weekly",
+  auto_assigned: true,
+  default_difficulty: 2)
 chat_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383954/CHANGER_LITIERE_CHAT_nkiilc.png')
 chat.image.attach(io: chat_image, filename: 'chat.png', content_type: 'image/png')
 
 
-battre_tapis  = Task.create!(name: "Battre le tapis",  description: "👟 Enlève tes chaussures la prochaine fois",
-recurrence: "daily", auto_assigned: true,  default_difficulty: 1)
+battre_tapis = Task.create!(
+  name: "Battre le tapis",
+  description: "👟 Enlève tes chaussures la prochaine fois",
+  recurrence: "daily",
+  auto_assigned: true,
+  default_difficulty: 1)
 battre_tapis_image = URI.open('https://res.cloudinary.com/dxrzdlwnz/image/upload/v1636383954/BATTRE_LE_TAPIS_mv6byq.png')
 battre_tapis.image.attach(io: battre_tapis_image, filename: 'tapis.png', content_type: 'image/png')
 
